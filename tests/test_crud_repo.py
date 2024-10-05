f1a7c042ebcbdec73d7f22054e53833a204b5d20
Inbasu <just.me.juncy@gmail.com>
2024-10-06 02:50:58 +0300

    modified:   pyproject.toml 	modified:   tests/test_crud_repo.py

--- tests/test_crud_repo.py
+++ tests/test_crud_repo.py
@@ -24,70 +24,31 @@ def teardown_module() -> None:
 
 
 @pytest_asyncio.fixture
-<<<<<<< HEAD
-async def session() -> AsyncSession:
-    """В fastAPI в Depend передается сессия,
-    и онa будет передваться в репозиторий внутри view,
-    поэтому в тестах try:...finally:..."""
-    return session_maker()
-=======
 async def session() -> AsyncGenerator[AsyncSession, Any]:
     async with session_maker() as connection:
         yield connection
->>>>>>> 17cbd6aaaaa29d5fe59d79bf775f235d8d1056c6
 
 
 @pytest.mark.asyncio
 async def test_get_item(session: AsyncSession) -> None:
-<<<<<<< HEAD
-    try:
-        word = await Dictionary.get_word(session=session, language="fr", value="bonjour")
-        await session.close()
-        assert word.__class__ == Word
-        assert word.language.name == "fr" if word is not None else False
-        assert word.translation[0].value == "hello" if word is not None else False
-    finally:
-        await session.close()
-=======
-    word = await Dictionary.get(session=session, language="fr", value="bonjour")
+    word = await Dictionary.get_word(session=session, language="fr", value="bonjour")
     assert word.__class__ == Word
     assert word.language.name == "fr" if word is not None else False
     assert word.translation[0].value == "hello" if word is not None else False
->>>>>>> 17cbd6aaaaa29d5fe59d79bf775f235d8d1056c6
 
 
 @pytest.mark.asyncio
 async def test_get_translation(session: AsyncSession, hello_word: Word) -> None:
-<<<<<<< HEAD
-    try:
-        bonjour = await Dictionary.get_translation(session, language="fr", value=hello_word.value)
-        assert hello_word.translation[0].value == bonjour.value if bonjour is not None else False
-        assert hello_word.translation[0].language.name == bonjour.language.name if bonjour is not None else False
-    finally:
-        await session.close()
-
-
-@pytest.mark.asyncio
-async def test_add_word(session: AsyncSession) -> None:
-    try:
-        await Dictionary.add_word(session, language="ru", value="привет")
-        created = await session.scalar(select(Word).where(Word.value == "привет").options(joinedload(Word.language)))
-        assert created.__class__ == Word
-        assert created.language.name == "ru"
-    finally:
-        await session.close()
-
-
-# @pytest.mark.asyncio
-# async def test_set_translation(session: AsyncSession, hello_word: Word) -> None:
-#     try:
-#         assert False
-#     finally:
-#         await session.close()
-=======
     bonjour = await Dictionary.get_translation(
         session, to="fr", language=hello_word.language.name, value=hello_word.value
     )
     assert hello_word.translation[0].value == bonjour.value if bonjour is not None else False
     assert hello_word.translation[0].language.name == bonjour.language.name if bonjour is not None else False
->>>>>>> 17cbd6aaaaa29d5fe59d79bf775f235d8d1056c6
+
+
+@pytest.mark.asyncio
+async def test_add_word(session: AsyncSession) -> None:
+    await Dictionary.add_word(session, language="ru", value="привет")
+    created = await session.scalar(select(Word).where(Word.value == "привет").options(joinedload(Word.language)))
+    assert created.__class__ == Word
+    assert created.language.name == "ru"
