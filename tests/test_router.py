9568ff4b2313f37bb00292e8c4d245f2cd60a6b4
inbasu <jsut.me.juncy@gmail.com>
2024-10-08 03:02:44 +0300

    modified:   tests/test_crud_repo.py 	modified:   tests/test_router.py

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 
 from routers import dictionary
@@ -6,16 +7,19 @@ from routers import dictionary
 from .fixtures import create_table, create_word_hello, drop_table  # noqa
 
 
+@pytest_asyncio.fixture(scope="session")
+async def client() -> AsyncClient:
+    return AsyncClient(transport=ASGITransport(app=dictionary), base_url="http://test")
+
+
 @pytest.mark.asyncio(loop_scope="session")
-async def test_translate_word() -> None:
-    async with AsyncClient(transport=ASGITransport(app=dictionary), base_url="http://test") as client:
-        response = await client.get("/word/en/hello/in/fr/")
+async def test_translate_word(client) -> None:
+    response = await client.get("/word/en/hello/in/fr/")
     assert response.json().get("value", "") == "bonjour"
     assert response.json().get("language", {}).get("name", "") == "fr"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_get_eng_words() -> None:
-    async with AsyncClient(transport=ASGITransport(app=dictionary), base_url="http://test") as client:
-        response = await client.get("/words/en/")
+async def test_get_eng_words(client: AsyncClient) -> None:
+    response = await client.get("/words/en/")
     assert len(response.json()) == 2
